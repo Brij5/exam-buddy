4b1bfed5bc73c1e999acae0e5669d9e1c650b929
Brij5 <brijiitk5@gmail.com>
2025-05-06 20:12:23 +0530

    Student dashboard and related features

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,6 +3,7 @@ import authReducer from './slices/authSlice';
 import examReducer from './slices/examSlice'; 
 import testAttemptReducer from './slices/testAttemptSlice';
 import examManagerReducer from './slices/examManagerSlice'; 
+import progressReducer from './slices/progressSlice'; 
 
 const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ const store = configureStore({
     exams: examReducer, 
     testAttempt: testAttemptReducer,
     examManager: examManagerReducer, 
+    progress: progressReducer,
     // Add other reducers here as the application grows
   },
   // Redux Toolkit includes redux-thunk middleware by default,
